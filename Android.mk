c1516e36f276dbabae89fd2e80019b4c0e2cd24a
hkotsch77 <holger.kotsch@gmx.de>
2019-11-09 18:38:17 +0100

    Upload

--- Android.mk
+++ Android.mk
@@ -10,6 +10,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := SamsungServiceMode
 LOCAL_JAVA_LIBRARIES := telephony-common
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 include $(BUILD_PACKAGE)
 
